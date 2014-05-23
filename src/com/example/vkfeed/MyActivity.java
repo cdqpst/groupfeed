7faa1748171f6e047e645a0768bb703169bbb857
AlexChe <cdqpst@gmail.com>
2014-05-23 18:16:53 +0300

    Add VkSKD

--- src/com/example/vkfeed/MyActivity.java
+++ src/com/example/vkfeed/MyActivity.java
@@ -1,7 +1,12 @@
 package com.example.vkfeed;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKSdkListener;
+import com.vk.sdk.VKUIHelper;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +16,24 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+//        VKSdk.initialize(VKSdkListener listener, String appId, VKAccessToken token);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        VKUIHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        VKUIHelper.onResume(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        VKUIHelper.onDestroy(this);
     }
 }
